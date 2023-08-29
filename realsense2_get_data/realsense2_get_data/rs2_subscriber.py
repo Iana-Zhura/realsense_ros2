867b3752ef109c346d5652e09251ca3100ba1b2a
Iana-Zhura <115732695+Iana-Zhura@users.noreply.github.com>
2023-08-29 11:38:37 +0200

    Add files via upload
    Final changes

--- realsense2_get_data/realsense2_get_data/rs2_subscriber.py
+++ realsense2_get_data/realsense2_get_data/rs2_subscriber.py
@@ -142,13 +142,7 @@ def main(args=None):
     rclpy.init(args=args)
 
     sub= MinimalSubscriber()
-    rclpy.spin(sub)
-
-    # Destroy the node explicitly
-    # (optional - otherwise it will be done automatically
-    # when the garbage collector destroys the node object)
-
-    
+    rclpy.spin(sub)   
     sub.destroy_node()
     rclpy.shutdown()
 
